da70b4732679a8aa88aed58dd0708fb866f410c1
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-10-18 14:25:13 +0200

    structure

---
+++ src/main/java/org/factoriaf5/projectohalloween/ProjectohalloweenApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.projectohalloween;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectohalloweenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectohalloweenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/factoriaf5/projectohalloween/ProjectohalloweenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.projectohalloween;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectohalloweenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
