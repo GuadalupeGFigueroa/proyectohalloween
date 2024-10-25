11cdc2f03c3779cda0c4b763f2492f4196dc295f
AnaBHernandez <[anahernandezfit@gmail.com]>
2024-10-25 14:17:51 +0200

    Model group completed

--- src/main/java/org/factoriaf5/projectohalloween/model/Groupskeletons.java
+++ src/main/java/org/factoriaf5/projectohalloween/model/Groupskeletons.java
@@ -1,5 +1,24 @@
 package org.factoriaf5.projectohalloween.model;
+import java.util.List;
 
 public class Groupskeletons {
+    private list <skeleton> skeletons;
+
+    public Groupskeletons(List<skeletons>skeletons){
+        this.skeletons =  skeletons;
     
+    }
+    public void Groupattack(Aiden hero) {
+        for (skeleton skeleton : skeletons ) {
+        hero.setLife_points(hero.getLife_points()- skeleton.getAttack_points());
+    }
+    }
+    public list<skeleton> getSkeletons() {
+        return skeletons;
+    }
+    public void setSkeletons(list<skeleton> skeletons) {
+        this.skeletons = skeletons;
+    }
+        
+
 }
