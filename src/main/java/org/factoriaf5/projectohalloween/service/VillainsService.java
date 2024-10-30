196df3c32137387fc4db0e6115985c4cd79d700e
NaudelynLucena <naulunau@gmail.com>
2024-10-30 15:15:24 +0100

    villains service file created

---
+++ src/main/java/org/factoriaf5/projectohalloween/service/VillainsService.java
@@ -0,0 +1,45 @@
+package org.factoriaf5.projectohalloween.service;
+import org.factoriaf5.projectohalloween.model.Villains;
+import org.factoriaf5.projectohalloween.repository.VillainsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VillainsService {
+    
+    private final VillainsRepository villainsRepository;
+
+    @Autowired
+    public VillainsService(VillainsRepository villainsRepository) {
+        this.villainsRepository = villainsRepository;
+    }
+
+    public List<Villains> getAllVillains() {
+        return villainsRepository.findAll();
+    }
+
+    public Optional<Villains> getVillainById(Long id) {
+        return villainsRepository.findById(id);
+    }
+
+    public Villains createVillain(Villains villain) {
+        return villainsRepository.save(villain);
+    }
+
+    public Optional<Villains> updateVillain(Long id, Villains villainDetails) {
+        return villainsRepository.findById(id).map(villain -> {
+            villain.setLife_points(villainDetails.getLife_points());
+            villain.setAttack_points(villainDetails.getAttack_points());
+            return villainsRepository.save(villain);
+        });
+    }
+
+    public boolean deleteVillain(Long id) {
+        return villainsRepository.findById(id).map(villain -> {
+            villainsRepository.delete(villain);
+            return true;
+        }).orElse(false);
+    }
+}
