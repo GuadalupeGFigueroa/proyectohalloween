9098c03144cdfb11e58ea501a583e7a54a88a188
Morty1904 <oscarendercraft@gmail.com>
2024-11-06 10:04:37 +0100

    modified ability

--- src/main/java/org/factoriaf5/projectohalloween/model/Ability.java
+++ src/main/java/org/factoriaf5/projectohalloween/model/Ability.java
@@ -1,55 +1,120 @@
 package org.factoriaf5.projectohalloween.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "Ability")
-
 public class Ability {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id_ability")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     private String name;
     private String type;
     private String effect;
-    public Ability(Long id, String name, String type, String effect) {
-        this.id = id;
+    private int cooldown; // Turnos de recarga
+    private int duration; // Duración del efecto en turnos
+    private int remainingCooldown; // Turnos restantes hasta que pueda usarse de nuevo
+    private int activeTurnsLeft; // Turnos restantes en los que el efecto está activo
+
+    public Ability() {}
+
+    public Ability(String name, String type, String effect, int cooldown, int duration) {
         this.name = name;
         this.type = type;
         this.effect = effect;
+        this.cooldown = cooldown;
+        this.duration = duration;
+        this.remainingCooldown = 0;
+        this.activeTurnsLeft = 0;
     }
+
+    public void activate() {
+        if (remainingCooldown == 0) {
+            activeTurnsLeft = duration;
+            remainingCooldown = cooldown;
+        }
+    }
+
+    public void decreaseCooldown() {
+        if (remainingCooldown > 0) {
+            remainingCooldown--;
+        }
+        if (activeTurnsLeft > 0) {
+            activeTurnsLeft--;
+        }
+    }
+
+    public boolean isAvailable() {
+        return remainingCooldown == 0;
+    }
+
+    public boolean isActive() {
+        return activeTurnsLeft > 0;
+    }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getType() {
         return type;
     }
+
     public void setType(String type) {
         this.type = type;
     }
+
     public String getEffect() {
         return effect;
     }
+
     public void setEffect(String effect) {
         this.effect = effect;
     }
 
-    
+    public int getCooldown() {
+        return cooldown;
+    }
 
+    public void setCooldown(int cooldown) {
+        this.cooldown = cooldown;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getRemainingCooldown() {
+        return remainingCooldown;
+    }
+
+    public void setRemainingCooldown(int remainingCooldown) {
+        this.remainingCooldown = remainingCooldown;
+    }
+
+    public int getActiveTurnsLeft() {
+        return activeTurnsLeft;
+    }
+
+    public void setActiveTurnsLeft(int activeTurnsLeft) {
+        this.activeTurnsLeft = activeTurnsLeft;
+    }
 }
--- src/main/java/org/factoriaf5/projectohalloween/model/Aiden.java
+++ src/main/java/org/factoriaf5/projectohalloween/model/Aiden.java
@@ -1,30 +1,75 @@
 package org.factoriaf5.projectohalloween.model;
 
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "Aiden")
-
 public class Aiden {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
-    @Column(name = "id_aiden")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
-    private int life_points;
-    private int attack_power;
+    private int lifePoints;
+    private int attackPower;
     private int score;
 
-    
-    public Aiden() {
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "aiden_id")
+    private List<Ability> abilities = new ArrayList<>();
+
+    public Aiden() {}
+
+    public Aiden(String name, int lifePoints, int attackPower, int score) {
+        this.name = name;
+        this.lifePoints = lifePoints;
+        this.attackPower = attackPower;
+        this.score = score;
     }
 
-    public Aiden(Long id, String name, int life_points, int attack_power, int level, int experience, int score) {
-        this.id = id;
-        this.name = name;
-        this.life_points = life_points;
-        this.attack_power = attack_power;
-        this.score = score;
+    public void addAbility(Ability ability) {
+        abilities.add(ability);
+    }
+
+    public void applyAbilityEffects() {
+        for (Ability ability : abilities) {
+            ability.decreaseCooldown();
+            if (ability.isActive()) {
+                switch (ability.getName()) {
+                    case "Golpe Potente":
+                        this.attackPower += 10;
+                        break;
+                    case "Escudo Protector":
+                        this.lifePoints += 5;  // O restar 5 al daño recibido
+                        break;
+                }
+            }
+        }
+    }
+
+    public boolean useAbility(String abilityName) {
+        for (Ability ability : abilities) {
+            if (ability.getName().equals(abilityName) && ability.isAvailable()) {
+                ability.activate();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getLifePoints() {
+        return lifePoints;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public List<Ability> getAbilities() {
+        return abilities;
     }
 
     public Long getId() {
@@ -43,20 +88,12 @@ public class Aiden {
         this.name = name;
     }
 
-    public int getLife_points() {
-        return life_points;
+    public void setLifePoints(int lifePoints) {
+        this.lifePoints = lifePoints;
     }
 
-    public void setLife_points(int life_points) {
-        this.life_points = life_points;
-    }
-
-    public int getAttack_power() {
-        return attack_power;
-    }
-
-    public void setAttack_power(int attack_power) {
-        this.attack_power = attack_power;
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
     }
 
     public int getScore() {
@@ -65,5 +102,9 @@ public class Aiden {
 
     public void setScore(int score) {
         this.score = score;
+    }
+
+    public void setAbilities(List<Ability> abilities) {
+        this.abilities = abilities;
     }
 }
