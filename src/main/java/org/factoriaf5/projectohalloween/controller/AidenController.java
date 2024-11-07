3a54de41f7a3990c2ef0b40c8e3b05c67338ec99
Óscar Menéndez Barrio <oscarendercraft@gmail.com>
2024-11-07 11:43:00 +0100

    reverted change of name in endpoint of aiden controller

--- src/main/java/org/factoriaf5/projectohalloween/controller/AidenController.java
+++ src/main/java/org/factoriaf5/projectohalloween/controller/AidenController.java
@@ -6,10 +6,17 @@ import org.factoriaf5.projectohalloween.model.Aiden;
 import org.factoriaf5.projectohalloween.service.AidenService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/aidens")
+@RequestMapping("/aiden")
 public class AidenController {
     private final AidenService aidenService;
 
